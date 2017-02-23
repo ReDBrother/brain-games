438bc7a70ac88c7bac897d40b89e00c630132161
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-02-23 13:12:23 +0300

    Update first game and index.js

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,31 +1,20 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import landing from '..';
+import brainGames from '..';
 
 const brief = 'Answer "yes" if number odd otherwise answer "no".\n';
-const userName = landing(brief);
-const answer = predicate => (predicate ? 'yes' : 'no');
-const game = (acc) => {
-  if (acc === 0) {
-    console.log(`Congratulations ${userName}!`);
-    return;
-  }
-
+const isOdd = predicate => (predicate ? 'yes' : 'no');
+const game = () => {
   const num = Math.floor(Math.random() * 100);
-  const isEven = num % 2 === 0;
+  const expectedAnswer = isOdd(num % 2 === 0);
   console.log(`Question: ${num}`);
   const currentAnswer = readlineSync.question(
     'Your answer: ', {
       limit: ['yes', 'no'],
       limitMessage: '$<lastInput> is not available.',
     });
-  if (answer(isEven) !== currentAnswer) {
-    console.log(`"${currentAnswer}" is wrong answer :(. Corrent answer was "${answer(isEven)}".`);
-    console.log(`Let's try again, ${userName}!`);
-    return;
-  }
-  game(acc - 1);
+  return { expectedAnswer, currentAnswer };
 };
 
-game(3);
+brainGames(brief, game);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import readlineSync from 'readline-sync';
 
-export default (leading) => {
+export default (leading, game) => {
   console.log('Welcome to the Brain Games!');
   if (leading !== undefined) {
     console.log(leading);
   }
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
-  return name;
+  if (game !== undefined) {
+    const iter = (acc) => {
+      if (acc === 0) {
+         console.log(`Congratulations, ${name}!`);
+         return;
+      }
+
+      const { expectedAnswer, currentAnswer } = game();
+      if (expectedAnswer === currentAnswer) {
+        console.log('Correct!');
+        iter(acc - 1);
+      } else {
+        console.log(`"${currentAnswer}" is wrong answer ;(. Correct answer was "${expectedAnswer}".`);
+        console.log(`Let's try again ${name}!`);
+      }
+    };
+
+    iter(3);
+  }
 };
