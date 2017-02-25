df91216d3b712cec2c0b74ba08e96462c3bd8b9e
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-02-25 04:21:35 +0300

    Seventh task

---
+++ src/games/brain-balance.js
@@ -0,0 +1,42 @@
+import brainGames from '..';
+
+const brief = 'Balance the given number.\n';
+const createNum = (acc, length) => {
+  if (length === 0) {
+    return acc;
+  }
+
+  const item = Math.floor(Math.random() * 10);
+  return createNum([item, ...acc], length - 1);
+};
+const isBalance = arr => Math.max(...arr) - Math.min(...arr) < 2;
+const getBalance = (num) => {
+  if (isBalance(num)) {
+    return num;
+  }
+
+  const iter = (arr) => {
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    const maxIndex = arr.indexOf(max);
+    const minIndex = arr.indexOf(min);
+    const newArr = [...arr];
+    newArr[minIndex] = Math.floor((max + min) / 2);
+    newArr[maxIndex] = (max + min) - newArr[minIndex];
+    return newArr;
+  };
+
+  const newNum = iter(num);
+  return getBalance(newNum);
+};
+const game = () => {
+  const length = Math.floor((Math.random() * 2) + 3);
+  const arr = createNum([], length);
+  const num = arr.join('');
+  const balancedItems = getBalance(arr);
+  const expected = balancedItems.sort().join('');
+  return { expected, items: num };
+};
+export default () => {
+  brainGames(brief, game);
+};
