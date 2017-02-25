5f2041ca82a612055c7d873270d66456894d8fe7
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-02-25 05:26:14 +0300

    ninth job

---
+++ src/games/brain-prime.js
@@ -0,0 +1,25 @@
+import brainGames from '..';
+
+const brief = 'Answer "yes" if number prime otherwise answer "no".\n';
+const isPrime = (num) => {
+  const iter = (acc) => {
+    if (Math.sqrt(num) < acc) {
+      return true;
+    } else if (num % acc === 0) {
+      return false;
+    }
+
+    return iter(acc + 1);
+  };
+
+  return iter(2);
+};
+const game = () => {
+  const num = Math.floor((Math.random() * 98) + 2);
+  const expected = isPrime(num) ? 'yes' : 'no';
+  return { expected, items: num };
+};
+
+export default () => {
+  brainGames(brief, game);
+};
